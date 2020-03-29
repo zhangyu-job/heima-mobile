5da24803de53c33e4a5e564bf67f6df4a583f454
zhangyu <zhangyu@example.com>
2020-03-29 09:32:38 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import App from './App.vue'
 import router from './router'
 import '@/permission' // 权限拦截模块
 import store from './store'
-import Vant from 'vant'
+import Vant, { Lazyload } from 'vant'
 import plugin from '@/utils/plugin'
 import 'vant/lib/index.less'
 import '@/styles/index.less' // 用来覆盖vant样式
 import 'amfe-flexible'
 Vue.use(Vant) // 注册vant组件   一旦注册  所以组件都可以使用
 Vue.use(plugin) // 注册组件  需要放置在Vue.use(Vant)之后  因为要在plugin中用vant函数
+Vue.use(Lazyload) // 注册懒加载指令  图片就可以搭配这个指令进行懒加载
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -19,13 +19,13 @@
               <!-- 根据封面类型判断单图 三图 无图 -->
               <!-- 三图 -->
               <div class="img_box" v-if="item.cover.type===3">
-                <van-image class="w33" fit="cover" :src="item.cover.images[0]" />
-                <van-image class="w33" fit="cover" :src="item.cover.images[1]" />
-                <van-image class="w33" fit="cover" :src="item.cover.images[2]" />
+                <van-image lazy-load class="w33" fit="cover" :src="item.cover.images[0]" />
+                <van-image lazy-load class="w33" fit="cover" :src="item.cover.images[1]" />
+                <van-image lazy-load class="w33" fit="cover" :src="item.cover.images[2]" />
               </div>
               <!-- 单图 -->
               <div class="img_box" v-if="item.cover.type===1">
-                <van-image class="w100" fit="cover" :src="item.cover.images[0]" />
+                <van-image lazy-load class="w100" fit="cover" :src="item.cover.images[0]" />
               </div>
 
               <div class="info_box">
